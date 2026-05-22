8f3b6edf512dee9b1e4b6ef135903295b97dd286
umageethikachinthakuntaartifixel <umageethika.chinthakunta@artifixel.com>
2026-05-22 13:26:32 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import "./index.css";
+import App from "./App";
+
+
+ReactDom.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
