129fa7e7431af43648cbcc91fd3b7b0401785233
umageethikachinthakuntaartifixel <umageethika.chinthakunta@artifixel.com>
2026-05-25 19:56:43 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import { Navbar } from "./components/Navbar";
 import { Sidebar } from "./components/Sidebar";
 
+import { MyCourses } from "./pages/MyCourses";
+import { NewCourses } from "./pages/NewCourses";
+
 function App() {
   return (
-    <div className="flex h-screen">
-      <Sidebar />
+    <BrowserRouter>
+      <div className="flex h-screen bg-gray-100">
+        <Sidebar />
 
-      <div className="flex flex-col flex-1">
-        <Navbar />
+        <div className="flex flex-col flex-1">
+          <Navbar />
+
+          <div className="flex-1 overflow-y-auto">
+            <Routes>
+              <Route path="/" element={<Navigate to="/courses" />} />
+
+              <Route path="/courses" element={<MyCourses />} />
+               <Route path="/courses/new" element={<NewCourses />} />
+            </Routes>
+          </div>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
